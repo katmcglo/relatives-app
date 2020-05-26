197f848220f6f517ea889a6d15084dcff1760b02
Ella Moeller <emoellervon@icloud.com>
2020-05-26 16:49:46 +0200

    routes of booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,11 @@
 class BookingsController < ApplicationController
+  def new
+    @family_members = FamilyMember.find(params[:family_members_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new
+
+  end
 end
