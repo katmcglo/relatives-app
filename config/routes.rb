1e5f5da52f7983e4619b9e114286a186f0311d35
Dareos Khalili <khalilidareos@gmail.com>
2020-05-26 16:08:50 +0200

    fixes as: :restaurant to family_member

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get "family_members/:id", to: "family_members#show", as: :restaurant
+  get "family_members/:id", to: "family_members#show", as: :family_member
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
