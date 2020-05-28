99478cd4dbaf929988552b2a53b6e608c136be13
Kat mcg <cathaumann@gmail.com>
2020-05-28 15:33:50 +0200

    THE SEARCH WORKS HOLY SHIT IN A HANDBASKET

--- app/controllers/family_members_controller.rb
+++ app/controllers/family_members_controller.rb
@@ -1,9 +1,19 @@
 class FamilyMembersController < ApplicationController
   def index
-    @family_members = policy_scope(FamilyMember).order(created_at: :desc)
+    if params[:query].present?
+      @family_members = policy_scope(FamilyMember).search_by_city(params[:query])
+    else
+      @family_members = policy_scope(FamilyMember).order(created_at: :desc).search_by_kinship(params[:kinship])
+    end
   end
+
+  # .search_by_kinship(params[:kinship])
 
   def show
     @family_member = FamilyMember.find(params[:id])
+  end
+
+  def new
+    @family_member = FamilyMember.new
   end
 end
--- app/models/family_member.rb
+++ app/models/family_member.rb
@@ -1,6 +1,9 @@
 class FamilyMember < ApplicationRecord
   belongs_to :user
   include PgSearch::Model
-  pg_search_scope :search_by_city_and_kinship,
-  against: [ :city, :kinship ]
+  pg_search_scope :search_by_city,
+  against: [ :city ],
+  using: [ :tsearch ]
+  pg_search_scope :search_by_kinship,
+  against: [ :kinship ]
 end
