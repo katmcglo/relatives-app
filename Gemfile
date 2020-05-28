6502e97df1fc6f07c40769ea1918ccb4d7263aa3
Kat mcg <cathaumann@gmail.com>
2020-05-28 12:18:19 +0200

    Added PGsearch gem for city and kinship, tested in console

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'faker'
+gem "pg_search"
 
 group :development, :test do
   gem 'pry-byebug'
--- app/models/family_member.rb
+++ app/models/family_member.rb
@@ -1,3 +1,6 @@
 class FamilyMember < ApplicationRecord
   belongs_to :user
+  include PgSearch::Model
+  pg_search_scope :search_by_city_and_kinship,
+  against: [ :city, :kinship ]
 end
