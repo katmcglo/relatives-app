d3e8e2768e43ebc5a605b92a5440e40afa4fac0a
Kat mcg <cathaumann@gmail.com>
2020-05-29 16:11:00 +0200

    commented out things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!, except: [:home, :index]
+  before_action :authenticate_user!, except: [:home, :index]
   # include Pundit
 
   # # Pundit: white-list approach.
