faad57524600d95f08eadd759cc22359a996a040
Dareos Khalili <khalilidareos@gmail.com>
2020-05-29 16:22:27 +0200

    adds cloudinary: images appear on Relatives cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,24 +4,24 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-require 'faker'
+# require 'faker'
 
-puts 'Creating 50 fake users...'
-fritz = User.create(email: "fritz@hawaii.com", password: "123456")
-49.times do
-  user = User.new(
-    email:    Faker::Internet.email,
-    password: "123456"
-  )
-  user.save!
-  family_member = FamilyMember.new(
-    name:    Faker::Name.name,
-    age: Faker::Number.between(from: 1, to: 110),
-    description: Faker::Lorem.paragraph(sentence_count: 2),
-    kinship: Faker::Relationship.familial,
-    user: user,
-    city: Faker::Address.city
-  )
-  family_member.save!
-end
-puts 'Finished!'
+# puts 'Creating 50 fake users...'
+# fritz = User.create(email: "fritz@hawaii.com", password: "123456")
+# 49.times do
+#   user = User.new(
+#    email:    Faker::Internet.email,
+#    password: "123456"
+#  )
+#  user.save!
+#  family_member = FamilyMember.new(
+#    name:    Faker::Name.name,
+#    age: Faker::Number.between(from: 1, to: 110),
+#    description: Faker::Lorem.paragraph(sentence_count: 2),
+#    kinship: Faker::Relationship.familial,
+#    user: user,
+#    city: Faker::Address.city
+#  )
+#  family_member.save!
+# end
+# puts 'Finished!'
